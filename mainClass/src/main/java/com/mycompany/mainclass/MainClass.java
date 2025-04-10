b77e5a7cf16b995ad5effae7d714c929bf0b58bb
rodri <rodri@Daniel>
2025-04-10 11:13:12 -0700

    cree el proyecto

--- mainClass/src/main/java/com/mycompany/mainclass/MainClass.java
+++ mainClass/src/main/java/com/mycompany/mainclass/MainClass.java
@@ -11,6 +11,6 @@ package com.mycompany.mainclass;
 public class MainClass {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola universo!");
     }
 }
